e777ace20be18975be5fab5393cf6f509e31d2e3
HospitalWebsite <luqmandarmawan532@gmail.com>
2021-10-12 21:58:24 +0700

    change the whole display

--- js/script.js
+++ js/script.js
@@ -36,25 +36,11 @@ document.querySelector("#close").onclick = () => {
   document.querySelector("#search-form").classList.remove("active");
 };
 
-var swiper = new Swiper(".home-slider", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 3500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  loop: true,
-});
-
 var swiper = new Swiper(".review-slider", {
   spaceBetween: 20,
   centeredSlides: true,
   autoplay: {
-    delay: 7500,
+    delay: 5000,
     disableOnInteraction: false,
   },
   loop: true,
